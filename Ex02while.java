10c241003ad2be03ae28a1e1769558c03a70f967
pedroenrikam <pedroenrikamaro@gmail.com>
2026-04-08 17:00:03 -0300

    Add files via upload

---
+++ Ex02while.java
@@ -0,0 +1,25 @@
+  void main() {
+Scanner sc = new Scanner(System.in);
+
+IO.println("digite um numero: ");
+int numerousuario = sc.nextInt();
+int resultado = 0;
+int contador = 1;
+while (contador <= numerousuario) {
+    IO.println(resultado);
+    resultado = resultado + contador;
+    contador++;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
