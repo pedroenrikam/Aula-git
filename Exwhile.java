3f29364e655a72648b86fe158bd3204f2b5c061c
pedroenrikam <pedroenrikamaro@gmail.com>
2026-04-08 15:13:59 -0300

    Add files via upload

---
+++ Exwhile.java
@@ -0,0 +1,10 @@
+public class Exwhile {public class ContagemRegressiva {
+    public static void main(String[] args) {
+        // Laço for começando em 10, indo até 0, subtraindo 1 a cada passo
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
+}
