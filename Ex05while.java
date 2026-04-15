0efd4fbfd2a8c3f454c99ce3e9d8ca9e8d55fb12
pedroenrikam <pedroenrikamaro@gmail.com>
2026-04-15 14:21:04 -0300

    Add files via upload

---
+++ Ex05while.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex05while {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        int num2=0;
+        int numero = 0;
+        num2 = sc.nextInt();
+        while (numero < 50) {
+        numero = numero + 2;
+                System.out.println(numero);
+            }
+            numero++;
+        }
+    }
+
+
+
+
+
+
+
+
